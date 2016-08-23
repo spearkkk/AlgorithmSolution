857428361e3f5532217adbe886e4613c632e3092
spearkkk <spearkkp@gmail.com>
2016-08-24 00:51:25 +0900

    2016.08.24. - Algospot : Endians done !!

---
+++ Algorithm/Endians/endians.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int cnt;
+
+	cin >> cnt;
+
+	for (int i = 0; i < cnt; ++i) {
+		unsigned int tmp;
+		short bytes[32] = { 0, };
+
+		cin >> tmp;
+		for (int j = 31; j >= 0; --j) {
+			if (tmp / (unsigned)pow(2, j) != 0) {
+				bytes[j] = 1;
+				tmp -= pow(2, j);
+			}
+		}
+
+		unsigned int rslt = 0;
+
+		for (int j = 0; j < 32; ++j) {
+			rslt += bytes[32 + (j % 8) - (8 * (j / 8 + 1))] * (unsigned)pow(2, j);
+		}
+
+		cout << rslt << endl;
+	}
+}
