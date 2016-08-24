9e7130173b63c007e223de5653ee9e5515fbaf73
spearkkk <spearkkp@gmail.com>
2016-08-24 09:26:01 +0900

    2016.08.24. - Algospot : Encrypt done !!

---
+++ Algorithm/Encrypt/encrypt.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string encrypt(string str);
+
+int main() {
+	int cnt;
+
+	cin >> cnt;
+	
+	for (int i = 0; i < cnt; ++i) {
+		string str;
+
+		cin >> str;
+		cout << encrypt(str) << endl;
+	}
+
+	return 0;
+}
+string encrypt(string str) {
+	string pre;
+	string post;
+	int idx = 0;
+
+	while(idx < str.length()) {
+		if (idx % 2 == 0) {
+			pre += str.at(idx);
+		}
+		else {
+			post += str.at(idx);
+		}
+		++idx;
+	}
+
+	return pre + post;
+}
